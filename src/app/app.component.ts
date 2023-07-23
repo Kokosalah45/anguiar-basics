efb7ba8e2159423cf926aba8555413f035e8b71a
kokosalah45 <kokp.amged@gmail.com>
2023-07-23 08:08:29 +0300

    addition of custom propreties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@
  @Component({selector : 'app-root' , templateUrl : './app.component.html' , styleUrls : ['./app.component.scss'] })
  export class AppComponent {
     title = "kerolous"
-
+    imgUrl = ""
+    handleImageChange(e : Event ){
+      this.imgUrl = (e.target as HTMLInputElement).value;
+    }
 
  }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
-  imgUrl = ""
-  handleImageChange(e : Event ){
-    this.imgUrl = (e.target as HTMLInputElement).value;
-  }
+  @Input() postImg = ""
+
 }
