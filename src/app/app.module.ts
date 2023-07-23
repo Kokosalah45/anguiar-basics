8e90a20efafb68c207b5fe77a932b755e49c2466
kokosalah45 <kokp.amged@gmail.com>
2023-07-23 08:05:28 +0300

    addition of prpperty binding | handling change | adding separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { PostComponent } from './post/post.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  declarations: [AppComponent, PostComponent],
+  bootstrap: [AppComponent] /* only used for the root component */
 })
 export class AppModule { }
